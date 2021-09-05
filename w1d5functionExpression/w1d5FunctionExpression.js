3c32120c678c3a98db3015f7248af44a1848d6b0
Abdulhamid Ibrahim <71563206+Hamid8542@users.noreply.github.com>
2021-09-05 14:07:08 -0500

    Create w1d5FunctionExpression.js

---
+++ w1d5functionExpression/w1d5FunctionExpression.js
@@ -0,0 +1,75 @@
+/**
+ * @Abdulhamid Ibrahim
+ * @param {*} w1d5functionExpressions 
+ * @returns 
+ */
+//1. Write a function, double, that takes a number and returns 2 times the number
+
+function double(num){
+ return 2*num;
+}
+console.log("Expected Result 10 :",double(5));
+
+//====================================================================================
+
+//  2. Write a function times100 that takes a number and returns 100 times the number.
+ 
+ function times100(num1){
+     return 100 * num1;
+ }
+ console.log("Expected Result 500 :",times100(5));
+//=====================================================================================
+/*
+3. Write a function, myMap, that takes an array and a function and returns a new array that has 
+the function applied to each element of the input array. Use your myMap function with your 
+double and times100 functions.
+*/
+ function myMap(array, doub, times) {
+     
+    console.log(doub(array));
+    console.log(times(array));
+}
+  
+   function double(array){
+        for(let i = 0; i < array.length; i++){
+            array[i] *= 2 ;
+   }
+        return array;
+}
+    function times100(array){
+        for(let i = 0; i < array.length; i++){
+            array[i] *= 100 ;
+   }
+        return array;
+}
+   console.log("Expected output [4,8,12] ", myMap ( [2,4,6], double, times100)); 
+   console.log("Expected output [300,500,700]", myMap ( [3,5,7], double, times100)); 
+
+//======================================================================================
+
+/*
+4. Demonstrate your myMap function with an anonymous function that triples the input value. 
+Write this as an anonymous function expression. Then write it using an arrow expression
+ */
+// Using function Expression
+let triples = function (array) {
+     
+    for(let i = 0; i < array.length; i++){
+            array[i] *= 3 ;
+   }
+     return array;
+}
+   console.log("Expected value [6,12,16]", triples( [2,4,6]));
+  
+//==============================================================
+
+//Using Arrow Function
+
+let triples = (array) => {
+for(let i = 0; i < array.length; i++){
+        array[i] *= 3 ;
+}
+     return array;
+ } ;
+ console.log("Expected value [6,12,16]", triples( [2,4,6]));
+
