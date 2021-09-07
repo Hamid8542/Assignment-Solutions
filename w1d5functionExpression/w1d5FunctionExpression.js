156c3969d8c360d08c6c92a445e8af6c305db30b
Abdulhamid Ibrahim <71563206+Hamid8542@users.noreply.github.com>
2021-09-06 20:57:43 -0500

    Update w1d5FunctionExpression.js

--- w1d5functionExpression/w1d5FunctionExpression.js
+++ w1d5functionExpression/w1d5FunctionExpression.js
@@ -1,78 +1,45 @@
-module.exports = {double, times100 , myMap };
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+*/ 
+module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
+
 /**
- * @Abdulhamid Ibrahim
- * @param {*} w1d5functionExpressions 
- * @returns 
+ *@returns {number} double the input
  */
-//1. Write a function, double, that takes a number and returns 2 times the number
-
-function doublee(num){
- return 2*num;
+function double(num){
+    return num * 2;
 }
-console.log("Expected Result (2 * 10) 20 :",doublee(10));
-console.log("Expected Result (2 * 0) 0 :",doublee(0));
-console.log("Expected Result (2 * -10) -20 :",doublee(-10));
 
-//====================================================================================
-// function double(array){
-//      let newArray = [];
-//     for(let i = 0; i < array.length; i++){
-//     newArray.push(array[i] * 2);
-//    }
-//    return newArray;
-// }
-//    console.log("Expected Result [4,8,12] :", double([2,4,6]));
-
-
-//  2. Write a function times100 that takes a number and returns 100 times the number.
- 
-function timess100(number){
-     return number * 100;
+/**
+ * @returns {number} 100 times the input
+ */
+ function times100(num){
+     return 100 * num;
  }
- console.log("Expected Result (100 * 10) 1000 :",timess100(10));
- console.log("Expected Result (100 * 0) 0 :",timess100(0));
- console.log("Expected Result (100 * -10) -1000 :",timess100(-10));
-//=====================================================================================
-/*
-3. Write a function, myMap, that takes an array and a function and returns a new array that has 
-the function applied to each element of the input array. Use your myMap function with your 
-double and times100 functions.
-*/
- function myMap(array, double) {
-     double();
-     return array;
-}
 
-console.log("Expected output will be  [-20,0,20,40] ", myMap([-10,0,10,20], double)); 
-   
-function double(array){
-        for(let i = 0; i < array.length; i++){
-            array[i] *= 2 ;
-   }
-        return array;
-}
 
-function myMap(array, times100){
-    times100(array);
-      return array;
-}
-
-    function times100(array){
-        for(let i = 0; i < array.length; i++){
-            array[i] *= 100 ;
-   }
-        return array;
-}
-  console.log("Expected output will be  [-20,0,20,40] ", myMap([-10,0,10,20], double)); 
-  console.log("Expected output [-1000,0,1000,2000]", myMap([-10,0,10,20], times100)); 
-
-//======================================================================================
-
-/*
-4. Demonstrate your myMap function with an anonymous function that triples the input value. 
-Write this as an anonymous function expression. Then write it using an arrow expression
+/**
+ * @returns {Array} creates a new array with function mapped to each element
  */
-// Using function Expression
+function myMap(array,doub){
+       let newArr = [];
+    for(let i = 0; i < array.length; i++){
+        let x = doub(array[i]);
+         newArr.push(x);  
+    }
+    return newArr; 
+}
+
+function myMap(array,times){
+    let newArr1 = [];
+ for(let i = 0; i < array.length; i++){
+     let x = times(array[i]);
+      newArr1.push(x);  
+ }
+ return newArr1; 
+}
+
 let triples = function (array) {
      
     for(let i = 0; i < array.length; i++){
@@ -82,16 +49,4 @@ let triples = function (array) {
 }
     console.log("Expected value [-3,0,30,60]", triples( [-1,0,10,20]));
   
-//==============================================================
-
-//Using Arrow Function
-
-let triples1 = (array) => {
-for(let i = 0; i < array.length; i++){
-        array[i] *= 3 ;
-}
-     return array;
- } ;
-
- console.log("Expected value [-3,0,30,60]", triples1( [-1,0,10,20]));
 
